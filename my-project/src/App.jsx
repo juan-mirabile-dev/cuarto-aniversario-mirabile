05f1076de062e84fee55b9e938b162de248f1635
QuickFixDev <juan.hernandez.mirabile@gmail.com>
2024-09-27 03:14:27 -0600

    add background

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -10,25 +10,27 @@ export default function App() {
   return (
     <>
       <Header />
-      <SectionTitle content="Concierto del Cuarto Aniversario de Mirabile." />
+      <div className="bg-neutral-900 rounded-md p-4 mb-4">
+        <SectionTitle content="Concierto del Cuarto Aniversario de Mirabile." />
 
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mb-8">
-        <div class="w-full h-64 overflow-hidden">
-          <img className="w-full h-full object-cover" src="png/mirabile_img_test.png" alt="Placeholder Image" />
-        </div>
-        <div className="text-start">
-          Han pasado cuatro años desde la fundación de Mirabile como ensamble coral, años en los que ustedes nos han impulsado a llegar cada vez más lejos.
-          Por ello, nos complace compartir que estamos próximos a celebrar nuestro cuarto aniversario.
-          Y qué mejor manera de hacerlo que haciendo lo que mejor sabemos: cantar.
-          En este concierto, interpretaremos dos obras reconocidas de la música clásica: el Concierto para Viola en Do menor, H. 104 de Carl Philipp Emanuel Bach
-          y el "Gloria in D major, RV 589" de Antonio Vivaldi.
-          <br />
-          <br />
-          Y tu, Ya tienes tus boletos?
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mb-8">
+          <div class="w-full h-64 overflow-hidden">
+            <img className="w-full h-full object-cover" src="png/mirabile_img_test.png" alt="Placeholder Image" />
+          </div>
+          <div className="text-start">
+            Han pasado cuatro años desde la fundación de Mirabile como ensamble coral, años en los que ustedes nos han impulsado a llegar cada vez más lejos.
+            Por ello, nos complace compartir que estamos próximos a celebrar nuestro cuarto aniversario.
+            Y qué mejor manera de hacerlo que haciendo lo que mejor sabemos: cantar.
+            En este concierto, interpretaremos dos obras reconocidas de la música clásica: el Concierto para Viola en Do menor, H. 104 de Carl Philipp Emanuel Bach
+            y el "Gloria in D major, RV 589" de Antonio Vivaldi.
+            <br />
+            <br />
+            Y tu, Ya tienes tus boletos?
+          </div>
         </div>
       </div>
 
-      <Members/>
+      <Members />
 
       <div className="mt-16 grid grid-cols-3 gap-4">
         <div className="col-span-full">
