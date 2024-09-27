88c0183ede36e14c3c53c9b8bbf3767bb2d83886
QuickFixDev <juan.hernandez.mirabile@gmail.com>
2024-09-27 03:10:49 -0600

    update members text size

--- my-project/src/components/Members.jsx
+++ my-project/src/components/Members.jsx
@@ -5,14 +5,14 @@ const Members = () => {
     return (
         <div className="mb-8 grid grid-cols-1 md:grid-cols-2 gap-4 text-start">
             <div className='mb-2'>
-                <p className="font-bold text-lg">Integrantes del coro</p>
+                <p className="font-bold text-md">Integrantes del coro</p>
                 <ul>
                     {Object.entries(choirMembers).map(([voicePart, members]) => (
                         <div className="mt-2" key={voicePart}>
-                            <p className="font-bold">{voicePart.charAt(0).toUpperCase() + voicePart.slice(1)}</p>
+                            <p className="font-bold text-sm">{voicePart.charAt(0).toUpperCase() + voicePart.slice(1)}</p>
                             <ul>
                                 {members.map((member, index) => (
-                                    <li key={index}>{member}</li>
+                                    <li className="text-xs" key={index}>{member}</li>
                                 ))}
                             </ul>
                         </div>
@@ -20,14 +20,14 @@ const Members = () => {
                 </ul>
             </div>
             <div className='mb-2'>
-                <p className="font-bold text-lg">Integrantes de la camerata</p>
+                <p className="font-bold text-md">Integrantes de la camerata</p>
                 <ul>
                     {Object.entries(camerataMembers).map(([instrumentPart, members]) => (
                         <div className="mt-2" key={instrumentPart}>
-                            <p className="font-bold">{instrumentPart.charAt(0).toUpperCase() + instrumentPart.slice(1)}</p>
+                            <p className="font-bold text-sm">{instrumentPart.charAt(0).toUpperCase() + instrumentPart.slice(1)}</p>
                             <ul>
                                 {members.map((member, index) => (
-                                    <li key={index}>{member}</li>
+                                    <li className="text-xs" key={index}>{member}</li>
                                 ))}
                             </ul>
                         </div>
